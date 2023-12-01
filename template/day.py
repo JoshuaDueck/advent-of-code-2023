c048ffed0a1427e2b6ae952fd1947f7c0ebd3b70
Joshua Dueck <joshua.dueck@gmail.com>
2023-12-01 08:10:19 -0600

    Initial commit

---
+++ template/day.py
@@ -0,0 +1,41 @@
+TEST1_ANSWER = 0
+TEST2_ANSWER = 0
+
+
+def main():
+    with open('testinput.txt') as f:
+        lines = f.readlines()
+        result = part1(lines)
+        if result == TEST1_ANSWER:
+            print("Test passed, continuing...")
+        else:
+            print("Test failed, \
+                    expected: {}, actual: {}".format(TEST1_ANSWER, result))
+            return
+
+        result = part2(lines)
+        if result == TEST2_ANSWER:
+            print("Test passed, continuing...")
+        else:
+            print("Test failed, \
+                    expected: {}, actual: {}".format(TEST2_ANSWER, result))
+            return
+
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+        print("Part 1:", part1(lines))
+        print("Part 2:", part2(lines))
+
+
+def part1(lines):
+    return TEST1_ANSWER
+
+
+def part2(lines):
+    return TEST2_ANSWER
+
+
+if __name__ == '__main__':
+    main()
+
